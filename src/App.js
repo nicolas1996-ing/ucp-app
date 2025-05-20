6413b8b90b1bf29514721319117b82d1c88c7b76
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-05-19 21:55:16 -0500

    feat: use basic hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const mainDiv = useRef(null)
+
+  useEffect(() => {
+    if (counter % 2 == 0) {
+      mainDiv.current.classList.add("screen_light");
+      mainDiv.current.classList.remove("screen_dark");
+    } else {
+      mainDiv.current.classList.add("screen_dark");
+      mainDiv.current.classList.remove("screen_light");
+    }
+  }, [counter]);
   return (
-    <div className="App">
+    <div className="App" id="main" ref={mainDiv}>
       <h1>¡Universidad Católica de Pereira !</h1>
       <p>Listado de integrantes - Proceso de Desarrollo de Software I</p>
       <p>José Nicolás Aristizabal Ramirez </p>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        incrementar visitas al sitio web
+      </button>
+      <span style={{ display: "block", margin: "5px" }}>{counter}</span>
     </div>
   );
 }
