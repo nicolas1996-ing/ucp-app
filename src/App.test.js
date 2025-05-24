4c02c2ee0d2f87bde9bcb6b538029f5205681d4f
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-05-24 11:05:01 -0500

    add jenkins file and unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Universidad Católica de Pereiraß', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/¡Universidad Católica de Pereira !/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Listado de integrantes - Proceso de Desarrollo de Software I', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Listado de integrantes - Proceso de Desarrollo de Software I/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
